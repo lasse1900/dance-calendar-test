a6517d2f95b65cf7621833468d39e3f9bc30703e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-09 09:55:54 +0200

    manual new build

---
+++ calendarBackend/build/precache-manifest.f0ddab48e827e8f86db15a8812b7573e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "88738518ea903efedf50",
+    "url": "/static/css/main.466a2353.chunk.css"
+  },
+  {
+    "revision": "88738518ea903efedf50",
+    "url": "/static/js/main.88738518.chunk.js"
+  },
+  {
+    "revision": "0bea90a6311340e47c3e",
+    "url": "/static/css/1.2a464d9f.chunk.css"
+  },
+  {
+    "revision": "0bea90a6311340e47c3e",
+    "url": "/static/js/1.0bea90a6.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70ea742f219141ea8807bb8d068842cf",
+    "url": "/static/media/argentine-tango_primary.70ea742f.jpg"
+  },
+  {
+    "revision": "0cf01ddc6442b16fb5afc855b3db033d",
+    "url": "/index.html"
+  }
+];
